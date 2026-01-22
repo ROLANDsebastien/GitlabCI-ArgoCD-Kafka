1b85ce4d9d1aaddcee56430ee8fbf098740b6436
echo  /Users/rolandsebastien/.zprofile <echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"'  /Users/rolandsebastien/.zprofile>
2026-01-22 08:06:22 +0100

    feat: Decrease VM disk and memory allocations and configure GitLab runner replicas and tags.

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -7,9 +7,9 @@ W2_NAME="k3s-worker2"
 
 echo "Creating VMs..."
 
-multipass launch --name $MASTER_NAME --cpus 1 --memory 2G --disk 20G
-multipass launch --name $W1_NAME --cpus 4 --memory 12G --disk 50G
-multipass launch --name $W2_NAME --cpus 2 --memory 8G --disk 40G
+multipass launch --name $MASTER_NAME --cpus 1 --memory 2G --disk 10G
+multipass launch --name $W1_NAME --cpus 4 --memory 8G --disk 30G
+multipass launch --name $W2_NAME --cpus 2 --memory 8G --disk 30G
 
 echo "Waiting for VMs..."
 sleep 20
